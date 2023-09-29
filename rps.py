485af61c9e19a99f2a12534db52cb1e728465be6
John Doe <patricktricenio549@gmail.com>
2023-09-29 22:57:30 +1300

    second

--- rps.py
+++ rps.py
@@ -6,33 +6,46 @@ class RPS(Enum):
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
-print('')
-
-
-
-playerchoice = input("Enter... \n1 for Rock,\n2 for paper, or\n3 for Scissors:\n\n")
-
-player = int(playerchoice)
-
-if player < 1 or player > 3: 
-    sys.exit("You must enter 1, 2, or 3")
     
-computerchoice = random.choice('123')
+playagain = True
+while playagain:
+   
 
-computer = int(computerchoice)
 
-print("")
-print("You chose" + str(RPS(player)).replace('RPS.','') + ".")
-print("Phyton chose" + str(RPS(computer)).replace('RPS.','') + ".")
-print("")
 
-if player == 1 and computer == 3:
-    print("You win! Woop Woop")
-elif player == 2 and computer == 1:
-    print("You win! Woop Woop")
-elif player == 3 and computer == 2:
-    print("You win! Woop Woop")
-elif player == computer:
-    print("Tie Game3!")
-else:
-    print("Phyton wins!")
+    playerchoice = input("\nEnter... \n1 for Rock,\n2 for paper, or\n3 for Scissors:\n\n")
+
+    player = int(playerchoice)
+
+    if player < 1 or player > 3: 
+        sys.exit("You must enter 1, 2, or 3")
+        
+    computerchoice = random.choice('123')
+
+    computer = int(computerchoice)
+
+    
+    print("\nYou chose" + str(RPS(player)).replace('RPS.','') + ".")
+    print("Phyton chose" + str(RPS(computer)).replace('RPS.','') + ".\n")
+   
+    if player == 1 and computer == 3:
+        print("You win! Woop Woop")
+    elif player == 2 and computer == 1:
+        print("You win! Woop Woop")
+    elif player == 3 and computer == 2:
+        print("You win! Woop Woop")
+    elif player == computer:
+        print("Tie Game3!")
+    else:
+        print("Phyton wins!")
+        
+    playagain = input("\nPlay again? \nY for Yes or \nQ to Quit \n\n")
+    
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("Congratulations")
+        print("Thank you for playing! \n")
+        playagain = False
+sys.exit(" System says Goodbye")
+        
